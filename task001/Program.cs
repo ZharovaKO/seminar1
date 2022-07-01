c6929970934f5152116492cbb85f2eddd7b7bda8
ZharovaKO <xkseo05@yandex.ru>
2022-07-01 20:07:02 +0300

    Задание 1

---
+++ task001/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine ("Введите первое число :");
+int firstNumber = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine ("Введите второе число :");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+ if (firstNumber > secondNumber)
+ {
+    Console.WriteLine(" наибольшее число: " + firstNumber);
+    Console.WriteLine(" наименьшее число: " + secondNumber);
+ }
+ else
+ {
+    Console.WriteLine(" наибольшее число: " + secondNumber);
+    Console.WriteLine(" наименьшее число: " + firstNumber);
+ }
+
