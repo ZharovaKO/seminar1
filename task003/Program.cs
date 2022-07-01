e17c11b2146a5b7c4bbba11b95ce4a8543960f8b
ZharovaKO <xkseo05@yandex.ru>
2022-07-01 21:52:25 +0300

    Задание 3

---
+++ task003/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine ("Введите число :");
+int number = Convert.ToInt32(Console.ReadLine()); 
+bool paritycheck (int number)
+{
+    return (number % 2 == 0);
+}
+Console.WriteLine(paritycheck(number));
