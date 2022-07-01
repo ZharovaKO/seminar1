ad69ca9684b475889bde7383f65b38f2b2699f90
ZharovaKO <xkseo05@yandex.ru>
2022-07-01 22:53:04 +0300

    Задание 4

---
+++ task004/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine()); 
+if(N % 2 == 0)
+{
+  while(N>=0)
+  {
+    Console.WriteLine(N);
+    N = N - 2;
+  }
+}
+else
+{
+  N = N - 1;
+  while(N>=0)
+  {
+    Console.WriteLine(N);
+    N = N - 2;
+  }
+}
