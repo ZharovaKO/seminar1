f80b8f126d2a9683b6edc52c799b30681bd42ded
ZharovaKO <xkseo05@yandex.ru>
2022-07-01 21:44:00 +0300

    Задание 2

---
+++ task002/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine ("Введите первое число :");
+int firstNumber = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine ("Введите второе число :");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Введите третье число :");
+int thirdNumber = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Максимальное число: ");
+Console.WriteLine(Math.Max(firstNumber, Math.Max(secondNumber, thirdNumber)));
+
